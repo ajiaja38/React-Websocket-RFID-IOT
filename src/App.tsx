9827d43fa31e7f50f272cac6ada7e1231ecb1eb7
M. Aji Perdana <ajicooljazz38@gmail.com>
2024-10-24 11:39:23 +0700

    feat: add websocket 112✨

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,33 @@ import io from "socket.io-client";
 import { IPayloadRMQ } from "./types/interface/IPayload.rmq";
 import { ResponseEntity } from "./types/interface/ResponseEntity";
 import Swal from "sweetalert2";
+import { IPayloadCase } from "./types/interface/payloadCase.interface";
 
 function App() {
   const [data, setData] = useState<IPayloadRMQ[]>([]);
+
+  useEffect(() => {
+    const socketPanic = io("wss://api-panic-button112.lskk.co.id/");
+    socketPanic.on("connect", (): void => {
+      console.log({ id: socketPanic.id });
+    });
+
+    socketPanic.on(
+      "case-112#COMPANY-a1461d6e-6341-4d31-a786-5f406cc4b9f5-2024",
+      (data: IPayloadCase): void => {
+        console.log("Received case 112");
+        console.log("Received case data:", data);
+      }
+    );
+
+    socketPanic.on(
+      "case-opd#COMPANY-5825eed5-65ac-45e8-b6ba-b55bacb651f7-2024",
+      (data: IPayloadCase): void => {
+        console.log("Received case OPD");
+        console.log("Received case data:", data);
+      }
+    );
+  }, []);
 
   useEffect((): (() => void) => {
     const socket = io("wss://presensi-service.lskk.co.id/");
@@ -16,13 +40,25 @@ function App() {
       console.log({ id: socket.id });
     });
 
-    socket.on("rfid-payload", (data: IPayloadRMQ): void => {
+    socket.on("rfid-payload-CMb80a", (data: IPayloadRMQ): void => {
       console.log("Received RFID data:", data);
       setData((prevState): IPayloadRMQ[] => [...prevState, data]);
     });
 
     socket.on(
       "response-user-85f335f1-c93b-4eb2-af31-66f29ff93b3d",
+      (data: ResponseEntity) => {
+        Swal.fire({
+          title: "Notification",
+          text: data.message,
+          icon: "success",
+          confirmButtonText: "OK",
+        });
+      }
+    );
+
+    socket.on(
+      "response-overtime-user-85f335f1-c93b-4eb2-af31-66f29ff93b3d",
       (data: ResponseEntity) => {
         Swal.fire({
           title: "Notification",
---
+++ src/types/interface/payloadCase.interface.ts
@@ -0,0 +1,8 @@
+export interface IPayloadCase {
+  guidCase: string;
+  guidCompany: string;
+  description: string;
+  imageUrl: string;
+  latitude: number;
+  longitude: number;
+}
